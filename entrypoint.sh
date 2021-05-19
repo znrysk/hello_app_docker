a354e87115e8e200cde5484db545a5ae06403063
atarubift <atarubift@gmail.com>
2021-05-19 14:49:04 +0900

    Add ebtrypoint

---
+++ entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+# Remove a potentially pre-existing server.pid for rails
+rf -f /hello_app/tmp/pids/server.pid
+
+# Them exec the container's main process (what's set as CMD in the Dockerfile).
+exec "$@"
